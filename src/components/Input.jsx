b9876121eb920f428d80e075ea8e48e6fb61e426
matheusmarques <marqueslarreamatheus@gmail.com>
2026-03-04 14:29:24 -0400

    commit inicial

---
+++ src/components/Input.jsx
@@ -0,0 +1,17 @@
+function Input(props) {
+  return (
+    <input className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" 
+       {...props}
+    />
+
+    );
+
+}
+
+export default Input;
+
+// type={props.type}
+//       placeholder={props.placeholder}
+
+//             value={props.value}
+//       onChange ={props.onChange}
